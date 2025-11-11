afbecfd113ace60e67266007b179b796dd698b41
NFAbricio <fabnunes014@gmail.com>
2025-11-11 12:08:12 -0300

    feat: aplication

--- api/api.go
+++ api/api.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"encoding/json"
 	"log/slog"
+	"math/rand/v2"
 	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/gorilla/handlers"
@@ -12,7 +15,17 @@ import (
 	"encurtador-go/utils"
 )
 
+// requestIDKey is the header key used to store the request ID.
 const requestIDKey = "X-Request-ID"
+
+type PostBody struct {
+	URL string `json:"url"`
+}
+
+type Response struct {
+	Error string `json:"error,omitempty"`
+	Data any   `json:"data,omitempty"`
+}
 
 // RequestID is a middleware that sets a unique request ID for each incoming HTTP request.
 func RequestID(next http.Handler) http.Handler {
@@ -31,13 +44,88 @@ func RequestID(next http.Handler) http.Handler {
 	})
 }
 
-func NewHandler() http.Handler {
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+func NewHandler(db map[string]string) http.Handler {
 	
 	router := mux.NewRouter()
 
+	// Logging middleware
+	logger := handlers.LoggingHandler(os.Stdout, router)
+
 	router.Use(handlers.RecoveryHandler(handlers.PrintRecoveryStack(true)))
 	router.Use(RequestID)
-	router.Use(utils.LoggerMiddleware(*logger))
+	router.Use(utils.JsonMiddleware)
+
+	router.HandleFunc("/api/shorten", handlePost(db)).Methods("POST")
+	router.HandleFunc("/{code}", handleGet(db)).Methods("GET")
+
+	return logger
 
 }
+
+// sendJSON sends a JSON response to the client with the given status code.
+func sendJSON(w http.ResponseWriter, resp Response, statusCode int){
+	// Set the content type to application/json
+	data, err := json.Marshal(resp)
+	
+	if err != nil {
+		slog.Error("failed to marshal json data", "error", err)
+		sendJSON(
+			w,
+			Response{Error: "internal server error"},
+			http.StatusInternalServerError, 
+		)
+		return
+	}
+	// Write the response
+	w.WriteHeader(statusCode)
+	if _, err := w.Write(data); err != nil {
+		slog.Error("failed to write response to client", "error", err)
+	}
+}
+
+const caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func generateShortURL() string {
+	const length = 8
+	bytes := make([]byte, length)
+
+	for i := range length {
+		bytes[i] = caracteres[rand.IntN(len(caracteres))]
+	}
+	return string(bytes)
+}
+
+func handlePost(db map[string]string) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		var body PostBody
+		// Decode the request body to URL
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			sendJSON(w, Response{Error: "invalid request body"}, http.StatusUnprocessableEntity)
+			return
+		}
+
+		if _, err := url.Parse(body.URL); err != nil {
+			sendJSON(w, Response{Error: "invalid url"}, http.StatusBadRequest)
+		}
+
+		code := generateShortURL()
+		// Store the URL in the database in memory
+		db[code] = body.URL
+
+		// Send the response
+		sendJSON(w, Response{Data: code}, http.StatusCreated)
+}}
+
+func handleGet(db map[string]string) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		// Get the code from the URL path
+		code := mux.Vars(r)["code"]
+		// Look up the original URL in the database
+		originalURL, ok := db[code]
+		// If the URL is not found, return a 404 error
+		if !ok {
+			sendJSON(w, Response{Error: "url not found"}, http.StatusNotFound)
+		}
+
+		http.Redirect(w, r, originalURL, http.StatusPermanentRedirect)
+}}
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "log/slog"
+import (
+	"encurtador-go/api"
+	"log/slog"
+	"net/http"
+	"time"
+)
 
 func main() {
 	if err := run(); err != nil {
@@ -11,5 +16,20 @@ func main() {
 }
 
 func run() error {
+	// In-memory database
+	db := make(map[string]string)
+	handler := api.NewHandler(db)	
+
+	serve := http.Server{
+		ReadTimeout: 10 * time.Second,
+		IdleTimeout: time.Minute,
+		WriteTimeout: 10 * time.Second,
+		Addr: ":8080",
+		Handler: handler,
+	}
+
+	if err := serve.ListenAndServe(); err != nil {
+		return err
+	}
 	return nil
 }
